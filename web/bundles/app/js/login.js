b8d802747a581ff4e0274e2fb5b688149a5672da
fhuszti <f.huszti@gmail.com>
2017-11-18 20:07:40 +0100

    Login and Password reset pages done

--- web/bundles/app/js/login.js
+++ web/bundles/app/js/login.js
@@ -9,24 +9,7 @@ $(function() {
 		$('#login_forgotten-link').on('click', function(e){
 			
 			//just load the reset request form into the modal body
-			modal_body.load($(e.target).data('action'), function() {
-				
-				//add a submit event to the modal form that was just loaded inside the body
-				form.on('submit', function(e){
-					e.preventDefault();
-
-					//we change the submit button to loading state
-					$('#login_forgotten-pass-modal-button').button('loading');
-					$('#login_forgotten-pass-modal-button>span:first-child').addClass('btn-loading');
-
-					var data = new FormData($(e.target));
-
-					//AJAX call using POST
-					$.post( $(e.target).attr('action'), data, function( response ) {
-					  	modal_body.html( response );
-					});
-				});
-			});
+			modal_body.load($(e.target).data('action'));
 		});
 	}
 
