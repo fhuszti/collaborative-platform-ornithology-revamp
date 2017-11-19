7bdf4dc08345b46e8c457ca732241e2a15231316
fhuszti <f.huszti@gmail.com>
2017-11-19 03:22:39 +0100

    Revamp of Observations management from admin panel done

--- web/bundles/app/js/admin.js
+++ web/bundles/app/js/admin.js
@@ -1,21 +1,20 @@
 $(function() {
 	
 	//generate a different button depending on the actual state of the observation
-	function generateStateBtn(state, id, admin_url) {
+	function generateStateBtn(state, urls) {
 		var button;
 
 		switch (state) {
-			//bootstrap == verbose
 			case 'valid':
-				button = $('<button class="btn btn-success"><span class="glyphicon glyphicon-ok-circle"></span> Validée</button><button class="btn btn-success dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button><ul class="dropdown-menu"><li><a href="'+admin_url+'obs/await/'+id+'" class="alert-warning"><span class="glyphicon glyphicon-refresh"></span> Mettre en attente</a></li><li><a href="'+admin_url+'obs/refuse/'+id+'" class="alert-danger"><span class="glyphicon glyphicon-remove-circle"></span> Refuser</a></li></ul>');
+				button = $('<button class="btn btn-success"><span class="glyphicon glyphicon-ok-circle"></span> Validée</button><button class="btn btn-success dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button><ul class="dropdown-menu"><li><a href="'+urls.await+'" class="alert-warning"><span class="glyphicon glyphicon-refresh"></span> Mettre en attente</a></li><li><a href="'+urls.refuse+'" class="alert-danger"><span class="glyphicon glyphicon-remove-circle"></span> Refuser</a></li></ul>');
 				break;
 
 			case 'refused':
-				button = $('<button class="btn btn-danger"><span class="glyphicon glyphicon-remove-circle"></span> Refusée</button><button class="btn btn-danger dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button><ul class="dropdown-menu"><li><a href="'+admin_url+'obs/validate/'+id+'" class="alert-success"><span class="glyphicon glyphicon-ok-circle"></span> Valider</a></li><li><a href="'+admin_url+'obs/await/'+id+'" class="alert-warning"><span class="glyphicon glyphicon-refresh"></span> Mettre en attente</a></li></ul>');
+				button = $('<button class="btn btn-danger"><span class="glyphicon glyphicon-remove-circle"></span> Refusée</button><button class="btn btn-danger dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button><ul class="dropdown-menu"><li><a href="'+urls.validate+'" class="alert-success"><span class="glyphicon glyphicon-ok-circle"></span> Valider</a></li><li><a href="'+urls.await+'" class="alert-warning"><span class="glyphicon glyphicon-refresh"></span> Mettre en attente</a></li></ul>');
 				break;
 
 			default:
-				button = $('<button class="btn btn-warning"><span class="glyphicon glyphicon-refresh"></span> En attente</button><button class="btn btn-warning dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button><ul class="dropdown-menu"><li><a href="'+admin_url+'obs/validate/'+id+'" class="alert-success"><span class="glyphicon glyphicon-ok-circle"></span> Valider</a></li><li><a href="'+admin_url+'obs/refuse/'+id+'" class="alert-danger"><span class="glyphicon glyphicon-remove-circle"></span> Refuser</a></li></ul>');
+				button = $('<button class="btn btn-warning"><span class="glyphicon glyphicon-refresh"></span> En attente</button><button class="btn btn-warning dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button><ul class="dropdown-menu"><li><a href="'+urls.validate+'" class="alert-success"><span class="glyphicon glyphicon-ok-circle"></span> Valider</a></li><li><a href="'+urls.refuse+'" class="alert-danger"><span class="glyphicon glyphicon-remove-circle"></span> Refuser</a></li></ul>');
 				break;
 		}
 
@@ -23,16 +22,11 @@ $(function() {
 	}
 
 	//init google maps
-	function initMap(param = null) {
-		//default parameter if non given
-		if (param == null) {
-			param = {lat: 36.8451807, lng: 10.1031312};
-		}
-	  	
-	  	//generate the map
+	function initMap(pos) {
+		//generate the map
 	  	var map = new google.maps.Map(document.getElementById('gmaps_canvas'), {
 	    	zoom: 8,
-	    	center: param
+	    	center: pos
 	  	});
 	  	
 	  	//responsive map
@@ -40,73 +34,48 @@ $(function() {
 	  	
 	  	//generate the marker at the right position
 	  	var marker = new google.maps.Marker({
-	    	position: param,
+	    	position: pos,
 	    	map: map
 	  	});
 	}
 
-	//manage the observation modal display
-	function manageObservationModal() {
+	//init the observation modal display
+	function initObservationModal() {
 		var observations = $('#observations .observation-row');
 
+        //init observation display load on modal opening for every observation
         observations.each(function(index, elmt) {
-            $(elmt).on('click', function(e) {
-            	e.preventDefault();
+            $(elmt).on('click', function() {
+            	//get current observation state and generate custom state button
+            	var state = $(this).data('state'),
+	            	urls = {
+	            		'validate': $(this).data('validate'),
+	            		'await': $(this).data('await'),
+	            		'refuse': $(this).data('refuse')
+	            	};
+            	generateStateBtn(state, urls).appendTo( $('#admin_obs-modal-state') );
 
-            	//get the observation data
-            	var image = $(this).data('image') ? $(this).data('image') : null,
-            		state = $(this).data('state'),
-            		id = $(this).data('id'),
-            		admin_url = $(this).data('url'),
-            		name = $(this).data('name'),
-            		date = $(this).data('date'),
-            		country = $(this).data('country'),
-            		comment = $(this).data('comment') ? $(this).data('comment') : null,
-            		lat = $(this).data('lattitude'),
-            		lng = $(this).data('longitude');
+            	//we load the right observation inside the modal
+				$('#admin_obs-modal-body').load($(this).data('url'), function() {
+					//get the observation position data
+	            	var gmaps_canvas = $('#gmaps_canvas'),
+	            		lat = parseFloat( gmaps_canvas.data('lattitude') ),
+	            		lng = parseFloat( gmaps_canvas.data('longitude') ),
+	            		pos = { lat: lat, lng: lng };
 
-            	//modal window elements
-            	var modal = $('#admin_obs-modal'),
-            		modal_state = $('#admin_obs-modal-state'),
-            		modal_left = modal.find('.admin_obs-modal-left-panel'),
-            		modal_right = $('#gmaps_canvas'),
-            		modal_comment = modal.find('.admin_obs-modal-comment-panel'),
-            		ul = $('<ul></ul>'),
-            		obsDiv = $(e.target);
-
-            	//we reset everything in case the modal has been opened and is filled with content already
-            	modal_state.html('');
-            	modal_left.html('');
-            	modal_right.html('');
-            	modal_comment.html('');
-
-            	var state_btn = generateStateBtn( state, id, admin_url );
-            	state_btn.appendTo(modal_state);
-
-            	//add the image if there is one
-            	if (image !== null) {
-	            	$('<img src="'+image+'" alt="'+name+'" />').appendTo(modal_left);
-	            }
-
-	            //add the informations list
-	            $('<li>'+name+'</li>').appendTo(ul);
-	            $('<li>'+date+'</li>').appendTo(ul);
-	            $('<li>'+country+'</li>').appendTo(ul);
-	            ul.appendTo(modal_left);
-
-	            //add the comment if there is one
-            	if (comment !== null) {
-	            	//<br> obligatoire, sinon le commentaire ne s'affiche pas
-	            	$('<hr />'+comment+'<br />').appendTo(modal_comment);
-	            }
-
-				//initialise the map once the modal is fully loaded and visible after a click on an observation
-				modal.on('shown.bs.modal', function () {
+	            	//initialise the map
 					if (typeof google === 'object' && typeof google.maps === 'object') {
-					 	initMap( {lat: parseFloat(lat), lng: parseFloat(lng)} );
+					 	initMap( pos );
 					}
 				});
             });
+        });
+
+        //empty modal when closing
+        $('#admin_obs-modal').on('hidden.bs.modal', function() {
+        	$('#admin_obs-modal-body').html('');
+
+        	$('#admin_obs-modal-state').html('');
         });
 	}
 
@@ -179,9 +148,9 @@ $(function() {
 
 
 
-	$.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCEL05YUkkeIBtLXDKcrZyM4kIkgbEOYS8");
+	$.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAQU7F5LE2WL4xxEpeUysxGqriN_RM36G0");
 
-	manageObservationModal();
+	initObservationModal();
 	manageUserDeleteConfirm();
 	manageUserEditModal();
 });
